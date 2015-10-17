182d8b5200fc42b39283f144eebce562d9c1ba04
caengcjd <1206557575@qq.com>
2015-10-17 14:18:09 +0800

    'ohchh'

--- app/view/auth/SelectProject.js
+++ app/view/auth/SelectProject.js
@@ -1,53 +1,3 @@
-<<<<<<< HEAD
-Ext.define("casco.view.auth.SelectProject",{
-    extend: 'Ext.window.Window',
-    xtype: 'selectProject',
-    
-    requires: [
-        'casco.view.auth.LoginController'
-    ],
-    
-    controller: 'login',
-    title: 'Select Project',
-    closable: false,
-    autoShow: true,
-    initComponent: function(){
-    	var store = Ext.create('casco.store.Projects');
-    	store.load({
-    		params: {
-    			user_id: JSON.parse(localStorage.user).id
-    		}
-    	});
-    	this.items = {
-	        xtype: 'form',
-	        bodyPadding: 10,
-	        reference: 'form',
-	        items: [{
-	        	xtype: 'combobox',
-	            editable: false,
-	            displayField: 'name',
-	            valueField: 'id',
-	            store: store,
-	            queryMode: 'local',
-	            emptyText: 'Select Project',
-	            allowBlank: false
-	        }],
-	        buttons: [ {
-	            text: 'Manage',
-	            listeners: {
-	                click: 'onManage'
-	            }
-	        },{
-	            text: 'Select',
-	            formBind: true,
-	            listeners: {
-	                click: 'onSelectClick'
-	            }
-	        }]
-	    };
-    	this.callParent();
-    }
-=======
 Ext.define("casco.view.auth.SelectProject",{
     extend: 'Ext.window.Window',
     xtype: 'selectProject',
@@ -97,5 +47,5 @@ Ext.define("casco.view.auth.SelectProject",{
 	    };
     	this.callParent();
     }
->>>>>>> 9f33e85b1f51e65073d256ba200429a4188d38aa
+
 });
