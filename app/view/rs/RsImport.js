b32935cc70d73ba317c50878c79e1ec6105ac9b5
caengcjd <1206557575@qq.com>
2016-02-26 16:00:36 +0800

    'all'

--- app/view/rs/RsImport.js
+++ app/view/rs/RsImport.js
@@ -1,24 +1,23 @@
 Ext.define('casco.view.rs.RsImport', {
 	extend : 'Ext.window.Window',
 	alias : 'widget.rs.rsimport',
-	
 	requires : [ 
 	             'casco.store.Rss', 
 	             'casco.store.Versions',
-	             //'casco.view.document.version.Create' 
+	             // 'casco.view.document.version.Create'
 	             ],
-	//uses : [],
+	// uses : [],
 
 	modal : true,
 	title : 'Document Import',
-	width : 400,
+	width : 500,
 	frame : true,
 	id : 'import-window',
 	viewModel : 'main',
 	initComponent : function() {
 		var me = this;
 		var headers=null;
-		//获取最近一次的列名吧
+		// 获取最近一次的列名吧
 		me.versions = new casco.store.Versions();
 		me.versions.load({
 			params:{
@@ -27,18 +26,18 @@ Ext.define('casco.view.rs.RsImport', {
 			},
 			callback:function(records, operation, success){
             
-			//必须要同步才能取值
-			//console.log(records[0].getData().responseText);
+			// 必须要同步才能取值
+			// console.log(records[0].getData().responseText);
 			if(records[0].getData().responseText!=""){
 			headers=me.versions.getAt(0).get('headers');
-		//	console.log(me.down('form').items.getAt(0));
+		// console.log(me.down('form').items.getAt(0));
             me.down('form').items.getAt(1).setValue(headers);
 			}else{
            
 		    if(me.type=="rs") me.down('form').items.getAt(1).setValue("description,implement,source,priority,contribute,category,allocation");
         else if(me.type="tc") me.down('form').items.getAt(1).setValue("test case description,safety,source,test method,pre_condition,test steps");
 			}
-		}//callback
+		}// callback
 		});
 		
 
@@ -47,7 +46,7 @@ Ext.define('casco.view.rs.RsImport', {
 			xtype : 'form',
 			bodyPadding : 10,
 			items : [ {
-				//version_id在这里啊
+				// version_id在这里啊
 				xtype : 'combobox',
 				fieldLabel : 'Version',
 
@@ -76,7 +75,8 @@ Ext.define('casco.view.rs.RsImport', {
 							Ext.Msg.alert('Notice','New Version: '+ input);
 							me.aflag = input;
 						}
-							//else Ext.Msg.alert('Notice','Exist Version: '+ input);
+							// else Ext.Msg.alert('Notice','Exist Version: '+
+							// input);
 					}
 				}
 			}, 
@@ -89,7 +89,6 @@ Ext.define('casco.view.rs.RsImport', {
 				fieldLabel:'Columns',
 				name:'column',
 				labelWidth:60,
-//				allowBlank:false,
 				width:'100%',
 				editable:'true',
 				value:headers,
@@ -98,10 +97,15 @@ Ext.define('casco.view.rs.RsImport', {
 						Ext.QuickTips.init();
 						Ext.QuickTips.register({
 							target:field.el,
-							text:'请用逗号分隔不同属性,大小写无关,单词之间请空一格'
-						})
-					}
-				}
+							text:'大小写无关,多个单词请空一格'
+						});
+					},
+					specialkey: function(field,e){    
+                        if (e.getKey() == e.ENTER){
+                        }  
+                    }  
+					
+				}// listenres
 				
 			},{
 				xtype : 'filefield',
@@ -131,49 +135,74 @@ Ext.define('casco.view.rs.RsImport', {
 			buttons : [ {
 				text : 'Import',
 				handler : function() {
-					var self = this;
+					var self = this;var obj=null;
 					var form = this.up('form').getForm();
-					//Ext.Msg.alert('Test',);
+					// Ext.Msg.alert('Test',);
 					if(me.aflag) form.findField('isNew').setValue(1);
 					if (form.isValid()) {
-						form.submit({//为什么一直为false
-							url : API + 'docfile',
-							waitMsg : 'Uploading file...',
-						    params:{'name':me.aflag},
-							success : function(form,action) {
-							 Ext.Msg.alert('Success',action.response.responseText);
-							
-							 // console.log(action);
-							},
-	                        failure: function(form,action) { 
-							
-	            
-							 var   versions = new casco.store.Versions();
-							 versions.load({
-						     synchronous: true,
-                             scope:this,
-					         callback:function(){
-					   		 
-							 // console.log((versions.getAt(0)));
-			                  Ext.Msg.alert('导入结果!',(versions.getAt(0).get('result')));
-							  // Ext.Msg.alert(record.get('result'));
-							 }
-							 });
+						// ajax请求
+						setTimeout(function ask_signal(){
+								Ext.Ajax.request({
+									url: API + 'docfile_pre',
+									method: 'get',
+									success: function(response,opts){
+									this.obj=response.responseText;
+										Ext.Msg.show({
+											title:'提示',
+											icon:Ext.MessageBox.INFO,
+											msg:'队列已有'+this.obj+'项任务正在进行,确定继续?',
+											width:300,
+											buttonText:{yes:'Continue',cancel:'Cancel'},
+											fn:function(buttonid){
+											
+											if(buttonid=='cancel'){self.up('window').destroy();return;}
+											form.submit({// 为什么一直为false
+												url : API + 'docfile',
+												waitMsg : 'Uploading file...',
+											    params:{'name':me.aflag},
+												success : function(form,action) {
+												 Ext.Msg.alert('Success',action.response.responseText);
+												
+												 // console.log(action);
+												},
+						                        failure: function(form,action) { 
+												 var versions = new casco.store.Versions();
+												 versions.load({
+											     synchronous: true,
+					                             scope:this,
+										         callback:function(){
+										   		 
+												 // console.log((versions.getAt(0)));
+								                  Ext.Msg.alert('导入结果!',(versions.getAt(0).get('result')));
+												  // Ext.Msg.alert(record.get('result'));
+												 }
+												 });
 
 
-							 self.up('window').destroy();
-                              var t=Ext.getCmp("docv-"+me.document_id);
-							  t.store.reload();
-    	                     /* var t = Ext.ComponentQuery.query("#tab-"+me.document_id)[0];
-    	          		      t.store_rs.reload();
-							  t.getView().refresh();
-							  */
-							  }//failure
-											  
-							 
-						});//submit
-					}
-				}
+												 self.up('window').destroy();
+					                              var t=Ext.getCmp("docv-"+me.document_id);
+												  t.store.reload();
+					    	                     /*
+													 * var t =
+													 * Ext.ComponentQuery.query("#tab-"+me.document_id)[0];
+													 * t.store_rs.reload();
+													 * t.getView().refresh();
+													 */
+												  }// failure
+											});// submit
+										}// function
+										});// show
+									}
+								});//ajax
+								},0001);
+							
+						 }//valid
+						
+					
+					
+						
+					}//handler
+			
 			} ],
 		} ];
 
